43f95bb0d669acc6ce717c89be1be247ee987a87
Ara <ara.akopyan@gmail.com>
2021-12-08 23:15:34 +0400

    fix: deleted console.log sentence at the end

--- Type_conversion.js
+++ Type_conversion.js
@@ -1,15 +1,14 @@
-"" + 1 + 0 // 10 => "" + String("1") + String("0")
-"" - 1 + 0 // -1 => Number("") - 1 + 0
-true + false // 1 => Number(true) + Number(false)  
-6 / "3" // 2 => 6 / Number("3")
-"2" * "3" // 6 => Number("2") * Number("3")
-4 + 5 + "px" // 9px => String(4 + 5) + "px"
-"$" + 4 + 5 // $45 => "$" + String(4) + String(5)
-"4" - 2 // 2 => Number("4") - 2
-"4px" - 2 // NaN => Number("4px") - 2 
-7 / 0 // Infinity
-" -9 " + 5 // " -9 5" => " -9 " + String(5)
-" -9 " - 5 // -14 => Number(" -9 ") - 5
-null + 1 // 1 => Number(null) + 1
-undefined + 1 // NaN => undefined haven't type
-console.log (undefined +1)
+1. "" + 1 + 0 // result: 10 ->  "" + String("1") + String("0")
+2. "" - 1 + 0 // result: -1 -> Number("") - 1 + 0
+3. true + false // result: 1 -> Number(true) + Number(false)  
+4. 6 / "3" // result: 2 -> 6 / Number("3")
+5. "2" * "3" // result: 6 -> Number("2") * Number("3")
+6. 4 + 5 + "px" // result: 9px -> String(4 + 5) + "px"
+7. "$" + 4 + 5 // result: $45 -> "$" + String(4) + String(5)
+8. "4" - 2 // result: 2 -> Number("4") - 2
+9. "4px" - 2 // result: NaN -> Number("4px") - 2 
+10. 7 / 0 // result: Infinity
+11. " -9 " + 5 // result: " -9 5" -> " -9 " + String(5)
+12. " -9 " - 5 // result: -14 -> Number("-9 ") - 5
+13. null + 1 // result: 1 -> Number(null) + 1
+14. undefined + 1 // result: NaN 
